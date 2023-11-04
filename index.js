aea122f180f480fdbfeb74525a2d0d1d832b22cd
midcondria <hyukkind@naver.com>
2023-11-04 23:14:13 +0900

    feature : 에러 처리 추가
    index.js
     - 입력값 없이 버튼을 누르면 기본값으로 1페이지 렌더링
     - 숫자 이외의 값 입력 시 기본 1페이지 렌더링
     - 숫자 입력 시 1 ~ 12 의 범위를 넘어선 페이지 입력 시 alert 창 띄움
     - 입력한 페이지에서 학생 수가 10명이 안되면 있는 만큼만 렌더링
     - catch 메서드로 에러처리하여 에러 발생 시 alert 창 띄움
    
    README.md
     - 작업 이력 추가 및 다음 목표 수정

--- index.js
+++ index.js
@@ -3,20 +3,25 @@ const imageArea = document.querySelector('.imageArea');
 const btn = document.querySelector('.magicBtn');
 const input = document.querySelector('#input')
 
-let bindedData = {};
-
 getData = function () {
-    const url = `https://api-blue-archive.vercel.app/api/characters?page=${Math.floor((input.value -1) /2) +1}`;
+    const inputValue = Number(input.value) ? Number(input.value) : 1;
+    const url = `https://api-blue-archive.vercel.app/api/characters?page=${Math.floor((inputValue -1) /2) +1}`;
+
+    if (inputValue > 12 || inputValue < 1) {
+        alert('페이지 입력은 1부터 12까지 가능합니다.');
+        return;
+    }
 
     fetch(url, {
         mode: 'cors'})
         .then((response) => response.json())
         .then((data) => {
-            bindedData = data.data
-
+            const bindedData = data.data
             imageArea.innerHTML = '';
-            if (input.value % 2 == 1) {
-                for (let i = 0; i < 10; i++) {
+
+            const trigger = (inputValue + 1)%2;
+            for (let i = 10*trigger; i < 10*trigger +10; i++) {
+                if (bindedData[i]) {
                     const student = document.createElement('div');
                     const name = document.createElement('div');
                     name.textContent = bindedData[i].name;
@@ -26,18 +31,11 @@ getData = function () {
                     student.append(image);
                     imageArea.append(student);
                 }
-            } else {
-                for (let i = 10; i < 20; i++) {
-                    const student = document.createElement('div');
-                    const name = document.createElement('div');
-                    name.textContent = bindedData[i].name;
-                    student.append(name);
-                    const image = document.createElement('img');
-                    image.setAttribute('src',bindedData[i].photoUrl);
-                    student.append(image);
-                    imageArea.append(student);
-                }
-            }
+            }            
+        })
+        .catch((error) => {
+            console.error('데이터를 불러오는 중 오류가 발생했습니다: ', error);
+            alert('데이터를 불러오는 중 오류가 발생했습니다.')
         });
 }
 
